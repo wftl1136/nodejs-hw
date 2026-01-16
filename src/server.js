bf74f16ada073d083a10b97ee234f439d5db4edb
Maksym Zhelezniak <zzzeleznyakmaxim@gmail.com>
2026-01-17 00:24:08 +0200

    fix full

--- src/server.js
+++ src/server.js
@@ -11,7 +11,14 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(pino());
+app.use(
+  pino({
+    transport: {
+      target: 'pino-pretty',
+      options: { colorize: true },
+    },
+  })
+);
 
 // Routes
 app.get('/notes', (req, res) => {
