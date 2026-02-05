7307c182c05a6b35f3297bfadb69095f511812d4
Maksym Zhelezniak <zzzeleznyakmaxim@gmail.com>
2026-02-05 18:34:27 +0200

    d

--- src/server.js
+++ src/server.js
@@ -1,13 +1,16 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 import { errors } from 'celebrate';
 
 import { connectMongoDB } from './db/connectMongoDB.js';
-import logger from './middleware/logger.js';
-import notFoundHandler from './middleware/notFoundHandler.js';
-import errorHandler from './middleware/errorHandler.js';
+import { logger } from './middleware/logger.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+import authRoutes from './routes/authRoutes.js';
 
 dotenv.config();
 
@@ -17,10 +20,13 @@ const { PORT = 3000 } = process.env;
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 app.use(logger);
 
-// Routes
-app.use('/notes', notesRoutes);
+// Routes (без префиксов)
+app.use(notesRoutes);
+app.use(userRoutes);
+app.use(authRoutes);
 
 // Not found middleware
 app.use(notFoundHandler);
